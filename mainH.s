8deb7a619c2534ed98cc1c59b2073f321ac3b4b2
jenbarillas <bar15307@uvg.edu.gt>
2016-10-13 18:40:51 -0600

    Update mainH.s

--- mainH.s
+++ mainH.s
@@ -23,92 +23,19 @@ main:
 	ldr r1,=halconX
 	str r0,[r1]
 	
-	@se pone el pin 18 como entrada para el boton
-
-	mov r0,#18
-	mov r1,#0
-	bl SetGpioFunction
-	
 	inicio:
-	
-	push {r0-r12}
-	ldr r0,=formatoD
-	ldr r1,=halconX
-	ldr r1,[r1]
-	bl printf
-	push {r0-r12}
 	
 	push {r0-r12}
 	//ldr r0,=pixelAddr 
 	bl pintarFondo			//Pinta el fondo
 	pop {r0-r12}
-
-
-
-	@si esta en 10 el contador lo regresa a 1 	
-	cmp r4,#10
-	moveq r4,#1	
 	
-	render1:
-		
-		x	  .req r4
-		y         .req r9
-		colour 	  .req r3
-		addrPixel .req r5
-		countByte .req r6
-		ancho	  .req r7
-		alto	  .req r8
-		
-		xPos	  .req r1
-		yPos	  .req r2
 		
 		/******************************************************************************/
 		
-		mov countByte,#0 				//Contador que cuenta la cantidad de bytes dibujados
-		ldr ancho,=halcon1Ancho
-		ldr ancho,[ancho]
-		ldr alto,=halcon1Alto
-		ldr alto,[alto]
-		ldr y,=0
-		ldr yPos, =200 //740
-		//Ciclo que dibuja filas
-		drawRow1:
-			ldr x,=0
-			ldr r12, =halconX
-			ldr xPos,[r12] //820
-			drawPixel1:
-				cmp x,ancho				//comparar x con el ancho de la imagen
-				bge end1
-				
-				ldr addrPixel,=halcon3	//Obtenemos la direccion de la matriz con los colores
-				ldrb colour,[addrPixel,countByte]	//Leer el dato de la matriz.
-				
-				ldr r0,=pixelAddr
-				ldr r0,[r0]
-				
-				ldr r11,=0
-				cmp colour,r11
-				 
-				push {r0-r12}
-				blne pixel				//Dibujamos el pixel. r1=x,r2=y,r3=colour
-				pop {r0-r12}
-				
-				add countByte,#1 		//Incrementamos los bytes dibujados
-				add x,#1 				//Aumenta el contador del ancho de la imagen
-				add xPos,#1 
-				
-				b drawPixel1
-				
-			
-		
-		end1:	
-			// aumentamos y
-			add y,#1
-			add ypos,#1 
-						
-			//Revisamos si ya dibujamos toda la imagen.
-			teq y,alto
-			bne drawRow1
+		push {r0-r12}	
+	bl pintarHalcon	
+	pop {r0-r12}
 		
 		push {r0-r12}
 				ldr r0,=500
@@ -117,69 +44,23 @@ main:
 		
 		push {r0-r12}
 	bl pintarFondo			//Pinta el fondo
-	bl boton
-	/*ldr r0,=halconX
+	
+	ldr r0,=halconX
 		ldr r0,[r0]
-		add r0,#10
+		add r0,#50
 	ldr r1,=halconX
 	str r0,[r1]
-	
-	ldr r0,=formatoD
-	ldr r1,=halconX
-	ldr r1,[r1]
-	bl printf*/
+
 	pop {r0-r12}
 	
 	
 		
 		/******************************************************************************/
 		
-		
-		
-		mov countByte,#0 				//Contador que cuenta la cantidad de bytes dibujados
-		ldr ancho,=halcon2Ancho
-		ldr ancho,[ancho]
-		ldr alto,=halcon2Alto
-		ldr alto,[alto]
-		ldr y,=0
-		ldr yPos, =200 //740
-		//Ciclo que dibuja filas
-		drawRow2:
-			ldr x,=0
-			ldr r12, =halconX
-			ldr xPos,[r12] //820
-			drawPixel2:
-			
-				cmp x,ancho				//comparar x con el ancho de la imagen
-				bge end2
-				
-				ldr addrPixel,=halcon2	//Obtenemos la direccion de la matriz con los colores
-				ldrb colour,[addrPixel,countByte]	//Leer el dato de la matriz.
-				
-				ldr r0,=pixelAddr
-				ldr r0,[r0]
-				
-				ldr r11,=0
-				cmp colour,r11
-				//beq drawPixel2
-				 
-				push {r0-r12}
-				blne pixel				//Dibujamos el pixel. r1=x,r2=y,r3=colour
-				pop {r0-r12}
-				
-				add countByte,#1 		//Incrementamos los bytes dibujados
-				add x,#1 				//Aumenta el contador del ancho de la imagen
-				add xPos,#1 
-				
-				b drawPixel2
-		end2:	
-			// aumentamos y
-			add y,#1
-			add ypos,#1 
-						
-			//Revisamos si ya dibujamos toda la imagen.
-			teq y,alto
-			bne drawRow2
+	push {r0-r12}	
+	bl pintarHalcon	
+	pop {r0-r12}	
+	
 			
 	push {r0-r12}
 				ldr r0,=500
@@ -188,67 +69,21 @@ main:
 	
 	push {r0-r12}
 	bl pintarFondo			//Pinta el fondo
-	bl boton
-	
-	/*ldr r0,=halconX
+	ldr r0,=halconX
 	ldr r0,[r0]
-		add r0,#10
+		add r0,#50
 	ldr r1,=halconX
 	str r0,[r1]		
 	
-	ldr r0,=formatoD
-	ldr r1,=halconX
-	ldr r1,[r1]
-	bl printf*/
-	
+
 	pop {r0-r12}
 						
 				
 	
 	/******************************************************************************/
-		
-		mov countByte,#0 				//Contador que cuenta la cantidad de bytes dibujados
-		ldr ancho,=halcon3Ancho
-		ldr ancho,[ancho]
-		ldr alto,=halcon3Alto
-		ldr alto,[alto]
-		ldr y,=0
-		ldr yPos, =200 //740
-		//Ciclo que dibuja filas
-		drawRow3:
-			ldr x,=0
-			ldr r12, =halconX
-			ldr xPos,[r12] //820
-			drawPixel3:
-				cmp x,ancho				//comparar x con el ancho de la imagen
-				bge end3
-				
-				ldr addrPixel,=halcon1	//Obtenemos la direccion de la matriz con los colores
-				ldrb colour,[addrPixel,countByte]	//Leer el dato de la matriz.
-				
-				ldr r0,=pixelAddr
-				ldr r0,[r0]
-				
-				ldr r11,=0
-				cmp colour,r11
-				 
-				push {r0-r12}
-				blne pixel			//Dibujamos el pixel. r1=x,r2=y,r3=colour
-				pop {r0-r12}
-				
-				add countByte,#1 		//Incrementamos los bytes dibujados
-				add x,#1 				//Aumenta el contador del ancho de la imagen
-				add xPos,#1 
-				
-				b drawPixel3
-		end3:	
-			// aumentamos y
-			add y,#1
-			add ypos,#1 
-						
-			//Revisamos si ya dibujamos toda la imagen.
-			teq y,alto
-			bne drawRow3
+	push {r0-r12}	
+	bl pintarHalcon	
+	pop {r0-r12}
 	
 	push {r0-r12}
 			ldr r0,=500
@@ -257,132 +92,63 @@ main:
 			
 	push {r0-r12}
 	bl pintarFondo			//Pinta el fondo
-	bl boton
-	/*ldr r0,=halconX
+	ldr r0,=halconX
 	ldr r0,[r0]
-		add r0,#10
+		add r0,#50
 	ldr r1,=halconX
 	str r0,[r1]
 	
-	ldr r0,=formatoD
-	ldr r1,=halconX
-	ldr r1,[r1]
-	bl printf*/
+	
 	
 	pop {r0-r12}
-	
-	
-	
-	
 
-	.unreq x		  
-	.unreq	y         
-	.unreq	colour 	  
-	.unreq	addrPixel 
-	.unreq	countByte 
-	.unreq	ancho	  
-	.unreq	alto	  
-	
-	.unreq	xPos	
-	.unreq	yPos	
+/******************ESTRELLA*******************/
+	estrella:
+		
+		
+		push {r0-r12}	
+		bl pintarEstrella	
+		pop {r0-r12}
+		
+		push {r0-r12}
+				ldr r0,=100
+				bl esperar			//Esperamos
+				pop {r0-r12}
+				
+		push {r0-r12}
+		bl pintarFondo			//Pinta el fondo
+		ldr r0,=estrellaY
+		ldr r0,[r0]
+			add r0,#50
+		ldr r1,=estrellaY
+		str r0,[r1]
+		
+		pop {r0-r12}
+		
+		@al llegar al final de la imagen
+		ldr r0,=tamano
+		ldr r1,[r0]
+		sub r1,r1,#50
+		str r1,[r0]
+		
+		cmp r1,#20
+		ldrlt r0,=estrellaY
+		movlt r1,#400
+		strlt r1,[r0]
+		
+		b estrella
 
 	b inicio
 	
 .data
-.global myloc
-myloc: .word 0
-.global pixelAddr, halconX, formatoD, nave
+.global pixelAddr, halconX, nave, formatoD
 pixelAddr: .word 0
+estrellaY: .word 0
 halconX: .word 500
-nave: .word 0
+nave:	.word 0
+tamano:	.word 664
+estrellas:	.word 0
+
 formatoD: .asciz "%d \n"
-
-/***************************************************************************/
-//BOTON
-
-.global boton
-boton:
-	mov r0,#18
-	bl GetGpio
-	cmp r0,#0
-	bne modificar
-	mov r0,#300
-	//bl esperar
-
-	modificar:
-		mov r0,#0
-		ldr r0,=halconX
-		ldr r0,[r0]
-		add r0,#50
-		ldr r1,=halconX
-		str r0,[r1]
-.global GetGpio
-GetGpio:
-	push {lr}
-	push {r5-r8}
-	mov r8,r0
-	
-	ldr r6,=myloc
-	ldr r0,[r6]
-	ldr r5,[r0,#0x34]
-	
-	mov r7,#1
-	lsl r7,r8
-	and r5,r5,r7
-	cmp r5,#0
-	moveq r0,#0
-	movgt r0,#1
-
-	pop {r5-r8}
-	pop {pc}
-
-.global SetGpioFunction
-SetGpioFunction:
-    pinNum .req r0
-    pinFunc .req r1
-	cmp pinNum,#53
-	cmpls pinFunc,#7
-	movhi pc,lr
-
-	push {lr}
-	mov r2,pinNum
-	.unreq pinNum
-	pinNum .req r2
-	@bl GetGpioAddress no se llama la funcion sino
-	ldr r6, =myloc
- 	ldr r0, [r6] @ obtener direccion 	
-	gpioAddr .req r0
-
-	functionLoop$:
-		cmp pinNum,#9
-		subhi pinNum,#10
-		addhi gpioAddr,#4
-		bhi functionLoop$
-
-	add pinNum, pinNum,lsl #1
-	lsl pinFunc,pinNum
-
-	mask .req r3
-	mov mask,#7					/* r3 = 111 in binary */
-	lsl mask,pinNum				/* r3 = 11100..00 where the 111 is in the same position as the function in r1 */
-	.unreq pinNum
-
-	mvn mask,mask				/* r3 = 11..1100011..11 where the 000 is in the same poisiont as the function in r1 */
-	oldFunc .req r2
-	ldr oldFunc,[gpioAddr]		/* r2 = existing code */
-	and oldFunc,mask			/* r2 = existing code with bits for this pin all 0 */
-	.unreq mask
-
-	orr pinFunc,oldFunc			/* r1 = existing code with correct bits set */
-	.unreq oldFunc
-
-	str pinFunc,[gpioAddr]
-	.unreq pinFunc
-	.unreq gpioAddr
-	pop {pc}
-	
-
-/***************************************************************************/
-
 
 
